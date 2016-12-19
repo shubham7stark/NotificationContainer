ad801e76a463f0375bab9bf7cb4e2627506f972d
Chirag Maheshwari <csoni111@gmail.com>
2016-12-19 19:46:20 +0530

    adds code to get notification data

--- app/src/main/java/com/sdsmdg/notificationcontainer/NLService.java
+++ app/src/main/java/com/sdsmdg/notificationcontainer/NLService.java
@@ -1,12 +1,22 @@
 package com.sdsmdg.notificationcontainer;
 
+import android.app.Notification;
+import android.app.PendingIntent;
 import android.content.BroadcastReceiver;
 import android.content.Context;
 import android.content.Intent;
 import android.content.IntentFilter;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.os.Bundle;
+import android.os.Parcel;
 import android.service.notification.NotificationListenerService;
 import android.service.notification.StatusBarNotification;
+import android.support.v4.app.NotificationCompat;
+import android.text.TextUtils;
 import android.util.Log;
+
+import java.util.ArrayList;
 
 /**
  * @author shubham
@@ -35,8 +45,76 @@ public class NLService extends NotificationListenerService{
         Log.i(TAG,"**********  onNotificationPosted");
         Log.i(TAG,"ID :" + sbn.getId() + "t" + sbn.getNotification().tickerText + "t" + sbn.getPackageName());
         Intent i = new  Intent("com.sdsmdg.notificationcontainer.NOTIFICATION_LISTENER_EXAMPLE");
-        i.putExtra("notification_event","onNotificationPosted :" + sbn.getPackageName() + "n");
+        i.putExtra("notification_event","onNotificationPosted :" + sbn.getPackageName());
         sendBroadcast(i);
+        Notification notification = sbn.getNotification();
+        Bundle extras = NotificationCompat.getExtras(notification);
+        String package_name = sbn.getPackageName();
+        String title = extras.getString(Notification.EXTRA_TITLE);
+        String title_big = extras.getString(Notification.EXTRA_TITLE_BIG);
+        Bitmap bmp = getNotificationIcon(extras);
+        String text_content = getNotificationText(extras);
+        String text_extended_content = getNotificationExtendedText(extras);
+        PendingIntent pendingIntent = notification.contentIntent;
+
+        ArrayList<NotificationCompat.Action> actions = new ArrayList<>();
+        int size = NotificationCompat.getActionCount(notification);
+        for (int j = 0; j < size; j++)
+            actions.add(NotificationCompat.getAction(notification, j));
+
+        if (bmp != null) {
+            Log.i(TAG, package_name+"|"+title+"|"+title_big+"|"+bmp.getHeight()+"|"+text_content+"|"+text_extended_content+"|"+pendingIntent.toString());
+        }
+    }
+
+    private String getNotificationText(Bundle extras) {
+        CharSequence chars =
+                extras.getCharSequence(Notification.EXTRA_TEXT);
+        if(!TextUtils.isEmpty(chars))
+            return chars.toString();
+        else if(!TextUtils.isEmpty((chars = extras.getString(Notification.EXTRA_SUMMARY_TEXT))))
+            return chars.toString();
+        else
+            return "";
+    }
+
+    private String getNotificationExtendedText (Bundle extras) {
+        CharSequence[] lines = extras.getCharSequenceArray(Notification.EXTRA_TEXT_LINES);
+        if(lines != null && lines.length > 0) {
+            StringBuilder sb = new StringBuilder();
+            for (CharSequence msg : lines)
+                if (!TextUtils.isEmpty(msg)) {
+                    sb.append(msg.toString());
+                    sb.append('\n');
+                }
+            return sb.toString().trim();
+        }
+        CharSequence chars = null;
+        if (android.os.Build.VERSION.SDK_INT >= android.os.Build.VERSION_CODES.LOLLIPOP) {
+            chars = extras.getCharSequence(Notification.EXTRA_BIG_TEXT);
+        }
+        if(!TextUtils.isEmpty(chars))
+            return chars.toString();
+        return "";
+    }
+
+    private Bitmap getNotificationIcon(Bundle extras) {
+        Bitmap icon;
+        icon = extras.getParcelable(Notification.EXTRA_LARGE_ICON);
+        if (icon != null ) {
+            return icon;
+        }
+        icon = extras.getParcelable(Notification.EXTRA_LARGE_ICON_BIG);
+        if (icon != null) {
+            return icon;
+        }
+        icon = extras.getParcelable(Notification.EXTRA_SMALL_ICON);
+        if (icon != null) {
+            return icon;
+        }
+        return null;
+//        icon = BitmapFactory.decodeResource(getResources(), R.mipmap.ic_launcher);
+//        return icon;
     }
 
     @Override
